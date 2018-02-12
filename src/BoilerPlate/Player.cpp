2588ca1acfb2f6597e237131f4617eef0790a39d
herrand15 <20165239@ce.pucmm.edu.do>
2018-02-12 00:18:14 -0600

    Adding vector4 and function protoypes

--- src/BoilerPlate/Player.cpp
+++ src/BoilerPlate/Player.cpp
@@ -8,11 +8,34 @@ void Player::Move(Vector2& mov) {
 	position->x += mov.x;
 	position->y += mov.y;
 
+	float x = position->x += mov.x;;
+	float y = position->y += mov.y;;
+
+	position->x = Warp(x, 1136/2, -1136/ 2);
+	position->y = Warp(y, 640/ 2, -640/ 2);
 }
+
+
+float Player::Warp(float position, float maxP, float minP) {
+	if (position < minP) {
+		return maxP - (minP - position);
+	}
+	if (position > maxP) {
+		return minP + (position - maxP);
+	}
+	return position;
+}
+
+void Player::setIsSpeedingUp() {
+  isSpeedingUp = true;
+}
+
 
 void Player::Render() {
 	glLoadIdentity();
+	
 	glTranslatef(position->x ,position->y, 0.0f);
+	
 
 	glBegin(GL_LINE_LOOP);
 	glVertex2f(25.0, 25.0);
@@ -20,6 +43,21 @@ void Player::Render() {
 	glVertex2f(-12.5, -12.5);
 	glVertex2f(-12.5, 12.5);
 	glEnd();
+	//Dibujar el fuego si la nave esta avanzando
+	if (isSpeedingUp==true) {
+		glBegin(GL_LINE_LOOP);
+		glVertex2f(-12.5, -12.5);
+		glVertex2f(4.56, -12.5);
+		glVertex2f(-3.27, -25.84);
+		glVertex2f(-9.25, - 17.3);
+		glVertex2f(-24.63, - 25.56);
+		glVertex2f(-18.23, - 9.61);
+		glVertex2f(-25.06, - 5.48);
+		glVertex2f(-12.5, 3.06);
+		isSpeedingUp = false;
+		glEnd();
+
+	}
 
 
 }
---
+++ src/BoilerPlate/_vector4.cpp
@@ -0,0 +1,178 @@
+#include "_vector4.h"
+#include <cmath>
+
+
+
+Vector4 Vector4::Origin = Vector4();
+
+Vector4::Vector4() {
+	x = 0.0f;
+	y = 0.0f;
+	z = 0.0f;
+	w = 0.0f;
+	length = 0.0f;
+}
+
+Vector4::Vector4(float x_, float y_, float z_, float w_)
+	: x(x_)
+	, y(y_)
+	, z(z_)
+	, length(0.0f)
+{
+	calculateLength();
+}
+
+float Vector4::calculateLength() {
+	return std::sqrt((x*x) + (y * y) + (z*z));
+
+}
+float Vector4::squaredLength() {
+	return (x*x) + (y*y) + (z*z);
+}
+
+float Vector4::normalizar() {
+	calculateLength();
+
+	float inverse = 1.0f / length; //para reducir la cantidad de operaciones que tendremos que realizar
+	x *= inverse;
+	y *= inverse;
+	z *= inverse;
+	w *= inverse;
+	return length;
+}
+
+Vector4& Vector4::operator=(Vector4& rightSide)
+{
+	
+	if (this == &rightSide) return *this;
+
+	x = rightSide.x;
+	y = rightSide.y;
+	z = rightSide.z;
+
+	return *this;
+}
+
+Vector4& Vector4::operator+=(Vector4& rightSide)
+{
+	x = x + rightSide.x;
+	y = y + rightSide.y;
+	z = z + rightSide.z;
+	w = w + rightSide.w;
+
+	return *this;
+}
+
+Vector4& Vector4::operator-=(Vector4& rightSide)
+{
+	x = x - rightSide.x;
+	y = y - rightSide.y;
+	z = z - rightSide.z;
+	w = w - rightSide.w;
+	return *this;
+}
+
+Vector4& Vector4::operator*=(Vector4& rightSide)
+{
+	x = x * rightSide.x;
+	y = y * rightSide.y;
+	z = z * rightSide.z;
+	w = w * rightSide.w;
+	return *this;
+}
+
+Vector4& Vector4::operator/=(Vector4& rightSide)
+{
+	if (rightSide.x == 0) {
+		throw "Can't divide by 0";
+	}
+	if (rightSide.y == 0) {
+		throw "Can't divide by 0";
+	}
+	if (rightSide.z == 0) {
+		throw "Can't divide by 0";
+	}
+	if (rightSide.w == 0) {
+		throw "Can't divide by 0";
+	}
+
+	x = x / rightSide.x;
+	y = y / rightSide.y;
+	z = z / rightSide.z;
+	w = w / rightSide.w;
+	return *this;
+}
+
+
+Vector4& Vector4::operator+(Vector4& rightSide)
+{
+	return Vector4(
+		x + rightSide.x,
+		z + rightSide.z,
+		y + rightSide.y,
+		w + rightSide.w);
+}
+
+Vector4& Vector4::operator-(Vector4& rightSide)
+{
+	return Vector4(
+		x - rightSide.x,
+		z - rightSide.z,
+		y - rightSide.y,
+		w - rightSide.w);
+}
+
+Vector4& Vector4::operator*(Vector4& rightSide)
+{
+	return Vector4(
+		x * rightSide.x,
+		z * rightSide.z,
+		y * rightSide.y,
+		w * rightSide.w);
+}
+
+Vector4& Vector4::operator/(Vector4& rightSide)
+{
+	if (rightSide.x == 0) throw "Division by zero is not defined!";
+	if (rightSide.y == 0) throw "Division by zero is not defined!";
+
+	return Vector4(
+		x / rightSide.x,
+		z / rightSide.z,
+		y / rightSide.y,
+		w / rightSide.w);
+}
+
+bool Vector4::operator==(Vector4& rightSide)
+{
+	return
+		x == rightSide.x &&
+		y == rightSide.y;
+}
+
+bool Vector4::operator!=(Vector4& rightSide)
+{
+	return
+		x != rightSide.x ||
+		y != rightSide.y ||
+		z != rightSide.z ||
+		w != rightSide.w;
+}
+
+Vector4 operator*(float scaleUnit, Vector4& rightSide)
+{
+	return Vector4(
+		scaleUnit * rightSide.x,
+		scaleUnit * rightSide.z,
+		scaleUnit * rightSide.y,
+		scaleUnit * rightSide.w);
+}
+
+Vector4 operator*(Vector4& leftSide, float scaleUnit)
+{
+	return Vector4(
+		scaleUnit * leftSide.x,
+		scaleUnit * leftSide.x,
+		scaleUnit * leftSide.y,
+		scaleUnit * leftSide.w);
+}
